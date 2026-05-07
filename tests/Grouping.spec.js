9e4f4618bfa2e469e2057e22e74e05581ec4bced
Viveka-123 <chitakulavivekanandana@gmail.com>
2026-05-07 15:25:02 +0530

    commit

--- tests/Grouping.spec.js
+++ tests/Grouping.spec.js
@@ -7,10 +7,12 @@ test("Test1", async({page})=>{
     await page.locator("#email").fill("chitakula@cixecnt.com")
     await page.waitForTimeout(5000)
 })
+})
 
+ test.describe("Group2", async()=>{
 test("Test2", async({page})=>{
     await page.goto("https://app.keka.com/Account/Login?ReturnUrl=%2F")
     await page.locator("#email").fill("chitakula@cixecnt.com")
      await page.locator("//*[text()='Continue with Mobile']").click()
 })
- })
+})
